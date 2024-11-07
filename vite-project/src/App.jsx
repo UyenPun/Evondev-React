bead80e932ab6b6ca7a3d15e8b7d5edb8e95de42
UyenPun <tranuyen2018@gmail.com>
2024-11-07 17:55:46 +0700

    Bài 7: Tìm hiểu JSX phần 2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <p>Hello</p>;
+  const name = "Uyen";
+
+  const fullName = (firstname, lastname) => {
+    return `${firstname} ${lastname}`;
+  };
+
+  return (
+    <div>
+      <p>{name}</p>
+      <p>Hello {fullName("Tran", "Uyen")}</p>
+    </div>
+  );
 }
 
 export default App;
