b5fe33c51cd0d27e72cb0e6ca03373a341b6ccea
UyenPun <tranuyen2018@gmail.com>
2024-11-10 23:02:48 +0700

    Bài 29: Thực hành game Tictactoe phần 2 -logic winner.

--- vite-project/src/components/tictactoe/Board.jsx
+++ vite-project/src/components/tictactoe/Board.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Cell from "./Cell";
+import { calculateWinner } from "../../helper";
 
 // create table gom 9 o
 const Board = () => {
   // Array(9).fill() -> [] => tạo ra 9 cái y nhau
+  const cells = [null, null, null, "X", "X", "X", null, null];
+  console.log(calculateWinner(cells));
 
   const array = [];
   return (
