83d28560138d7a89b32ef676f5cae9b5e86c98a3
UyenPun <tranuyen2018@gmail.com>
2024-11-17 18:27:19 +0700

    Bài 44: Sử dụng styled-components phần 1

--- vite-project/src/components/card/Card.jsx
+++ vite-project/src/components/card/Card.jsx
@@ -1,14 +1,51 @@
 import React from "react";
 import styled from "styled-components";
+
+/**
+const StyledCard = styled.tag(h1, h2, div, span, strong, a, p, section, article...)``
+CSS-in-JS
+  **/
+
+const StyledCard = styled.div`
+  position: relative;
+`;
+
+const CardImage = styled.div`
+  height: 400px;
+  width: 400px;
+  border-radius: 8px;
+`;
+
+const CardImg = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+`;
+
+const CardContent = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: white;
+  z-index: 10;
+  border-radius: 20px;
+  padding: 20px;
+  bottom: 0;
+`;
+
 const Card = (props) => {
   return (
-    <div>
-      <img
-        src='https://cdn.dribbble.com/users/2400293/screenshots/16527147/media/f079dc5596a5fb770016c4ea506cd77b.png?resize=1000x750&vertical=centerr'
-        alt=''
-      />
+    <StyledCard>
+      <CardImage>
+        <CardImg
+          src='https://cdn.dribbble.com/users/2400293/screenshots/16527147/media/f079dc5596a5fb770016c4ea506cd77b.png?resize=1000x750&vertical=centerr'
+          alt=''
+        />
+      </CardImage>
 
-      <div>
+      <CardContent>
         <div>
           {/* Right */}
           <div>
@@ -27,8 +64,8 @@ const Card = (props) => {
           <h3>Cosmic Perspective</h3>
           <span>12,000 PSL</span>
         </div>
-      </div>
-    </div>
+      </CardContent>
+    </StyledCard>
   );
 };
 
